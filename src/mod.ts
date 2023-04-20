1c7e26dd267c585f97b9fa08bb9d3f7bf5ec11eb
Jon D. Crowder <rccommandotac@gmail.com>
2023-04-19 23:00:39 -0500

    update mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -1,8 +1,8 @@
 
-export * from "./ecs/component.js";
 export * from "./ecs/components/camera.js";
 export * from "./ecs/components/mesh.js";
 export * from "./ecs/components/transform.js";
+export * from "./ecs/component.js";
 export * from "./ecs/entity.js";
 export * from "./graph/scene.js";
 export * from "./math/color.js";
@@ -11,6 +11,7 @@ export * from "./math/matrix.js";
 export * from "./math/quaternion.js";
 export * from "./math/transform.js";
 export * from "./math/vector.js";
+export * from "./evt.js";
 export * from "./mesh.js";
 export * from "./meshbuilder.js";
 export * from "./shader.js";
